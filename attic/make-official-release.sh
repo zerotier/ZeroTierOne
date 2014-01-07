afec5b0f2e046b7949f28d598133d8300c60cf2a
Adam Ierymenko <adam.ierymenko@gmail.com>
2014-01-07 14:12:57 -0800

    Official release maker... again only of academic interest to others.

--- attic/make-official-release.sh
+++ attic/make-official-release.sh
@@ -23,6 +23,34 @@ if [ "$#" -ne 2 ]; then
 	exit 1
 fi
 
+if [ ! -e zerotier-idtool ]; then
+	echo "Unable to find zerotier-idtool in current directory."
+	exit 1
+fi
+
 secret="$1"
 dest="$2"
 
+if [ ! -e "$secret" ]; then
+	echo "Can't find $secret"
+	exit 1
+fi
+if [ ! -d "$dest" ]; then
+	echo "Can't find $dest directory"
+	exit 1
+fi
+
+for inst in `ls ZeroTierOneInstaller-*-*-*_*_*`; do
+	echo "Found installer: $inst"
+	nfo="$inst.nfo"
+	rm -f "$nfo"
+	echo "tss=`date`" >>"$nfo"
+	echo "vMajor=`echo $inst | cut -d - -f 4 | cut -d _ -f 1`" >>"$nfo"
+	echo "vMinor=`echo $inst | cut -d - -f 4 | cut -d _ -f 2`" >>"$nfo"
+	echo "vRevision=`echo $inst | cut -d - -f 4 | cut -d _ -f 3`" >>"$nfo"
+	echo "signedBy=`cat $secret | cut -d : -f 1`" >>"$nfo"
+	echo "ed25519=`./zerotier-idtool sign $secret $inst`" >>"$nfo"
+	echo "url=http://download.zerotier.com/$inst" >>"$nfo"
+	cp $inst $nfo $dest
+	rm -f $nfo
+done
