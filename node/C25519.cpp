4788d911ade3e74792f63349d7bd4bf257f76003
Adam Ierymenko <adam.ierymenko@gmail.com>
2014-01-14 08:27:59 -0800

    Remove ZT1 GPL copyright from the top of code that is basically all Daniel Bernstein's

--- node/C25519.cpp
+++ node/C25519.cpp
@@ -1,29 +1,13 @@
+// Code taken from NaCl by D. J. Bernstein and others
+
 /*
- * ZeroTier One - Global Peer to Peer Ethernet
- * Copyright (C) 2012-2013  ZeroTier Networks LLC
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * --
- *
- * ZeroTier may be used and distributed under the terms of the GPLv3, which
- * are available at: http://www.gnu.org/licenses/gpl-3.0.html
- *
- * If you would like to embed ZeroTier into a commercial application or
- * redistribute it in a modified binary form, please contact ZeroTier Networks
- * LLC. Start here: http://www.zerotier.com/
- */
+Matthew Dempsky
+Public domain.
+Derived from public domain code by D. J. Bernstein.
+*/
+
+// Modified very slightly for ZeroTier One by Adam Ierymenko
+// (no functional changes)
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -38,14 +22,6 @@ namespace ZeroTier {
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-
-// Code taken from NaCl by D. J. Bernstein and others
-
-/*
-Matthew Dempsky
-Public domain.
-Derived from public domain code by D. J. Bernstein.
-*/
 
 #define crypto_int32 int32_t
 #define crypto_uint32 uint32_t
--- node/Poly1305.cpp
+++ node/Poly1305.cpp
@@ -1,29 +1,8 @@
 /*
- * ZeroTier One - Global Peer to Peer Ethernet
- * Copyright (C) 2012-2013  ZeroTier Networks LLC
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * --
- *
- * ZeroTier may be used and distributed under the terms of the GPLv3, which
- * are available at: http://www.gnu.org/licenses/gpl-3.0.html
- *
- * If you would like to embed ZeroTier into a commercial application or
- * redistribute it in a modified binary form, please contact ZeroTier Networks
- * LLC. Start here: http://www.zerotier.com/
- */
+20080912
+D. J. Bernstein
+Public domain.
+*/
 
 #include "Poly1305.hpp"
 
@@ -31,12 +10,6 @@ namespace ZeroTier {
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-
-/*
-20080912
-D. J. Bernstein
-Public domain.
-*/
 
 static void add(unsigned int h[17],const unsigned int c[17])
 {
