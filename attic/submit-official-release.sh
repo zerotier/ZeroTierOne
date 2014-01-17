dab124dfb937f40033613e821da914d75f578637
Adam Ierymenko <adam.ierymenko@gmail.com>
2014-01-17 12:57:31 -0800

    VERSION 0.6.9: more UI, installation, and packaging work...
    Another release leading up to official binary releases... not much to the core,
    but quite a bit of work on the UI, installation, and such.
    
    This version will build and run on OSX 10.6 while previous versions would fail
    due to a missing getifmaddrs() function.

--- attic/submit-official-release.sh
+++ attic/submit-official-release.sh
@@ -3,3 +3,7 @@
 # Completely useless to outsiders. :)
 
 scp ZeroTierOneInstaller-*-*-*_*_* nyarlathotep.zerotier.com:/www/download.zerotier.com/htdocs
+
+if [ -f 'ZeroTier One.dmg' ]; then
+	scp 'ZeroTier One.dmg' nyarlathotep.zerotier.com:/www/download.zerotier.com/htdocs/dist
+fi
