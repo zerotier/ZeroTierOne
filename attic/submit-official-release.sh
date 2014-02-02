f7fbc6f6330d49b4e91d6388a433657318e163e4
Adam Ierymenko <adam.ierymenko@gmail.com>
2014-02-01 22:31:52 -0800

    Remove submit script.

--- attic/submit-official-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Completely useless to outsiders. :)
-
-scp ZeroTierOneInstaller-*-*-*_*_* nyarlathotep.zerotier.com:/www/download.zerotier.com/htdocs
-
-if [ -f 'ZeroTier One.dmg' ]; then
-	scp 'ZeroTier One.dmg' nyarlathotep.zerotier.com:/www/download.zerotier.com/htdocs/dist
-fi
