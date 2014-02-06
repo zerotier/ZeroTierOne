d0e5da28840f9d521afdc3c6aeaaab88275d4977
Adam Ierymenko <adam.ierymenko@zerotier.com>
2014-02-05 16:38:54 -0800

    Fix copyright notice.

--- windows/ZeroTierOne/ZeroTierOneService.h
+++ windows/ZeroTierOne/ZeroTierOneService.h
@@ -1,21 +1,29 @@
-/****************************** Module Header ******************************\
-* Module Name:  SampleService.h
-* Project:      CppWindowsService
-* Copyright (c) Microsoft Corporation.
-* 
-* Provides a sample service class that derives from the service base class - 
-* CServiceBase. The sample service logs the service start and stop 
-* information to the Application event log, and shows how to run the main 
-* function of the service in a thread pool worker thread.
-* 
-* This source is subject to the Microsoft Public License.
-* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
-* All other rights reserved.
-* 
-* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
-* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-\***************************************************************************/
+/*
+ * ZeroTier One - Global Peer to Peer Ethernet
+ * Copyright (C) 2012-2013  ZeroTier Networks LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --
+ *
+ * ZeroTier may be used and distributed under the terms of the GPLv3, which
+ * are available at: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * If you would like to embed ZeroTier into a commercial application or
+ * redistribute it in a modified binary form, please contact ZeroTier Networks
+ * LLC. Start here: http://www.zerotier.com/
+ */
 
 #pragma once
 
