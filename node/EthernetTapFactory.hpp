6a174483d8a4a29b913c3e6945513bb800d443c0
Adam Ierymenko <adam.ierymenko@gmail.com>
2014-07-18 17:26:08 -0700

    Ethernet tap factory interface.

---
+++ node/EthernetTapFactory.hpp
@@ -0,0 +1,74 @@
+/*
+ * ZeroTier One - Global Peer to Peer Ethernet
+ * Copyright (C) 2011-2014  ZeroTier Networks LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --
+ *
+ * ZeroTier may be used and distributed under the terms of the GPLv3, which
+ * are available at: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * If you would like to embed ZeroTier into a commercial application or
+ * redistribute it in a modified binary form, please contact ZeroTier Networks
+ * LLC. Start here: http://www.zerotier.com/
+ */
+
+#ifndef ZT_ETHERNETTAPFACTORY_HPP
+#define ZT_ETHERNETTAPFACTORY_HPP
+
+#include <stdint.h>
+
+#include "MAC.hpp"
+
+namespace ZeroTier {
+
+class EthernetTap;
+
+/**
+ * Ethernet tap factory
+ *
+ * This serves up tap implementations for a given platform. It should never be
+ * deleted until the Node using it is shut down, since doing so may invalidate
+ * any tap devices it manages.
+ *
+ * Using a factory pattern will faciliatate packaging ZeroTier as a library,
+ * as well as moving toward a design that makes unit testing the entire app
+ * quite a bit easier.
+ */
+class EthernetTapFactory
+{
+public:
+	EthernetTapFactory() {}
+	virtual ~EthernetTapFactory() {}
+
+	/**
+	 * Create / open an Ethernet tap device
+	 *
+	 * On some platforms (Windows) this can be a time-consuming operation.
+	 *
+	 * @param mac MAC address
+	 * @param mtu Device MTU
+	 * @param metric Interface metric (higher = lower priority, may not be supported on all OSes)
+	 * @param nwid ZeroTier network ID
+	 * @param desiredDevice Desired system device name or NULL for no preference
+	 * @param friendlyName Friendly name of this interface or NULL for none (not used on all platforms)
+	 * @return EthernetTap instance or NULL on failure
+	 */
+	virtual EthernetTap *get(const MAC &mac,unsigned int mtu,unsigned int metric,uint64_t nwid,const char *desiredDevice,const char *friendlyName) = 0;
+};
+
+} // namespace ZeroTier
+
+#endif
