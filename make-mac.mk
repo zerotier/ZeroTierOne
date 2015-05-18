f48509d50cf62ed593ae86fb4e8ee1a0750f7690
Adam Ierymenko <adam.ierymenko@gmail.com>
2015-05-17 20:28:09 -0700

    Mac script to get HTTP proxy settings -- will be used by Mac updater.

--- make-mac.mk
+++ make-mac.mk
@@ -8,7 +8,6 @@ ARCH_FLAGS=-arch x86_64
 
 include objects.mk
 OBJS+=osdep/OSXEthernetTap.o 
-#TESTNET_OBJS=testnet/SimNet.o testnet/SimNetSocketManager.o testnet/TestEthernetTap.o 
 
 # Disable codesign since open source users will not have ZeroTier's certs
 CODESIGN=echo
@@ -62,10 +61,6 @@ selftest: $(OBJS) selftest.o
 	$(CXX) $(CXXFLAGS) -o zerotier-selftest selftest.o $(OBJS) $(LIBS)
 	$(STRIP) zerotier-selftest
 
-#testnet: $(TESTNET_OBJS) $(OBJS) testnet.o
-#	$(CXX) $(CXXFLAGS) -o zerotier-testnet testnet.o $(OBJS) $(TESTNET_OBJS) $(LIBS)
-#	$(STRIP) zerotier-testnet
-
 # Requires that ../Qt be symlinked to the Qt root to use for UI build
 #mac-ui: FORCE
 #	mkdir -p build-ZeroTierUI-release
@@ -79,20 +74,10 @@ clean:
 	rm -rf *.dSYM build-* *.pkg *.dmg *.o node/*.o controller/*.o service/*.o osdep/*.o ext/http-parser/*.o ext/lz4/*.o ext/json-parser/*.o zerotier-one zerotier-idtool zerotier-selftest zerotier-cli ZeroTierOneInstaller-*
 
 # For our use -- builds official signed binary, packages in installer and download DMG
-official: FORCE
-	make -j 4 ZT_OFFICIAL_RELEASE=1
-	./buildinstaller.sh
+#official: FORCE
+#	make -j 4 ZT_OFFICIAL_RELEASE=1
+#	./buildinstaller.sh
 #	make mac-dmg ZT_OFFICIAL_RELEASE=1
-
-#mac-dmg:	FORCE
-#	mkdir -p build-ZeroTierOne-dmg
-#	cd build-ZeroTierOne-dmg ; ln -sf /Applications Applications
-#	cp -a "build-ZeroTierUI-release/ZeroTier One.app" build-ZeroTierOne-dmg/
-#	rm -f /tmp/tmp.dmg
-#	hdiutil create /tmp/tmp.dmg -ov -volname "ZeroTier One" -fs HFS+ -srcfolder ./build-ZeroTierOne-dmg
-#	hdiutil convert /tmp/tmp.dmg -format UDZO -o "ZeroTier One.dmg"
-#	$(CODESIGN) -f -s $(CODESIGN_CERT) "ZeroTier One.dmg"
-#	rm -f /tmp/tmp.dmg
 
 # For those building from source -- installs signed binary tap driver in system ZT home
 install-mac-tap: FORCE
