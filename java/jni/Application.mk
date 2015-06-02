74f7d5377d26c721b17b5a5380793c26df81b2e3
Grant Limberg <grant.limberg@red.com>
2015-06-01 20:01:37 -0700

    Enable ZT_TRACE and more verbose build output

--- java/jni/Application.mk
+++ java/jni/Application.mk
@@ -1,3 +1,4 @@
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86
 APP_STL := gnustl_static
-APP_CPPFLAGS += -Wall -fPIE -fstack-protector -fexceptions
+APP_CPPFLAGS += -Wall -fPIE -fstack-protector -fexceptions -DZT_TRACE
+
