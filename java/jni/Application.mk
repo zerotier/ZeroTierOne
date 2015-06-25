bfb152f53f528934583ee76437453a005610a7ea
Grant Limberg <glimberg@gmail.com>
2015-06-24 20:31:22 -0700

    configure the NDK to build all supported ABIs and package them up in the jar

--- java/jni/Application.mk
+++ java/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86
 APP_STL := gnustl_static
-APP_CPPFLAGS += -Wall -fPIE -fstack-protector -fexceptions -DZT_TRACE
-
+APP_CPPFLAGS += -Wall -fPIE -fstack-protector -fexceptions
+APP_PLATFORM := android-14
+APP_ABI := all
