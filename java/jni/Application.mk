a408e5f6858a0e2f13c27e56f203b35f64ab07e7
Grant Limberg <glimberg@gmail.com>
2015-07-08 17:59:53 -0700

    set ZT_NO_TYPE_PUNNING and -O3 for Android builds

--- java/jni/Application.mk
+++ java/jni/Application.mk
@@ -1,5 +1,5 @@
 NDK_TOOLCHAIN_VERSION := clang
 APP_STL := c++_static
-APP_CPPFLAGS := -O0 -fPIC -fPIE -fvectorize -Wall -fstack-protector -fexceptions -fno-strict-aliasing
+APP_CPPFLAGS := -O3 -fPIC -fPIE -fvectorize -Wall -fstack-protector -fexceptions -fno-strict-aliasing -DZT_NO_TYPE_PUNNING=1
 APP_PLATFORM := android-14
 APP_ABI := all
