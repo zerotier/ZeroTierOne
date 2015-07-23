a493fc23f4305f80d93f0879324aa87f536a1a90
Adam Ierymenko <adam.ierymenko@gmail.com>
2015-07-23 13:05:18 -0700

    Fix for make-linux: detect whether CC/CXX were explicitly overridden, and if not then use the gcc/clang selection logic. Otherwise ?= breaks this.

--- make-linux.mk
+++ make-linux.mk
@@ -18,8 +18,14 @@
 #
 
 # Automagically pick clang or gcc, with preference for clang
-CC?=$(shell if [ -e /usr/bin/clang ]; then echo clang; else echo gcc; fi)
-CXX?=$(shell if [ -e /usr/bin/clang++ ]; then echo clang++; else echo g++; fi)
+# This is only done if we have not overridden these with an environment or CLI variable
+ifeq ($(origin CC),default)
+	CC=$(shell if [ -e /usr/bin/clang ]; then echo clang; else echo gcc; fi)
+endif
+ifeq ($(origin CXX),default)
+	CXX=$(shell if [ -e /usr/bin/clang++ ]; then echo clang++; else echo g++; fi)
+endif
+
 INCLUDES=
 DEFS=
 LDLIBS?=
