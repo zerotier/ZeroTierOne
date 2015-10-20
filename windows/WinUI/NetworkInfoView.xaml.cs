9ba5c91d4a2f67a146310c5544c7b385bd1f1d8a
Grant Limberg <glimberg@gmail.com>
2015-10-19 21:17:18 -0700

    initial layout for network info

---
+++ windows/WinUI/NetworkInfoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinUI
+{
+    /// <summary>
+    /// Interaction logic for NetworkInfoView.xaml
+    /// </summary>
+    public partial class NetworkInfoView : Page
+    {
+        public NetworkInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
