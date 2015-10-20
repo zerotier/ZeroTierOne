56267b2aed72998eece335b838b081ad06f10756
Grant Limberg <glimberg@gmail.com>
2015-10-19 20:29:06 -0700

    setting icon and a few other settings

--- windows/WinUI/Properties/AssemblyInfo.cs
+++ windows/WinUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinUI")]
+[assembly: AssemblyTitle("ZeroTier One")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinUI")]
+[assembly: AssemblyCompany("ZeroTier, Inc")]
+[assembly: AssemblyProduct("ZeroTier One")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
