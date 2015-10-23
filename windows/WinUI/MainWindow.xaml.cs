7e635d37ad6cfa1195c8f141dd533564e478cc36
Grant Limberg <glimberg@gmail.com>
2015-10-23 15:49:04 -0700

    only accept hex characters for network id

--- windows/WinUI/MainWindow.xaml.cs
+++ windows/WinUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -51,11 +52,10 @@ namespace WinUI
 
         }
 
-        private void joinNetworkID_TextChanged(object sender, TextChangedEventArgs e)
+        private void OnNetworkEntered(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[0-9a-fxA-FX]");
+            e.Handled = !regex.IsMatch(e.Text);
         }
-
-
     }
 }
