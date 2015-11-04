a95ff21aafa793e5282f32b77b893da3f42a4a57
Adam Ierymenko <adam.ierymenko@gmail.com>
2015-11-04 15:09:19 -0800

    test stuff

--- tests/http/big-test-kill.sh
+++ tests/http/big-test-kill.sh
@@ -4,6 +4,6 @@
 
 export PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin
 
-pssh -h big-test-hosts -i -t 0 -p 256 "sudo docker ps -aq | xargs -r sudo docker rm -f"
+pssh -h big-test-hosts -i -OUserKnownHostsFile=/dev/null -OStrictHostKeyChecking=no -t 0 -p 256 "sudo docker ps -aq | xargs -r sudo docker rm -f"
 
 exit 0
