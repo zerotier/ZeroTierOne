e4d1aba3f82c1265579b0b9dfba22903e34ae7d1
Adam Ierymenko <adam.ierymenko@gmail.com>
2015-11-16 16:19:24 -0800

    Use new OSX tap version, and update OSX install scripts.

--- ext/installfiles/mac/launch.sh
+++ ext/installfiles/mac/launch.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 zthome="/Library/Application Support/ZeroTier/One"
-export PATH="/bin:/usr/bin:/sbin:/usr/sbin:$zthome"
-
-# If the app has been deleted, uninstall the service
-cd "$zthome"
-if [ -L './shutdownIfUnreadable' -a ! -f "`readlink ./shutdownIfUnreadable`" ]; then
-	rm -f /tmp/ZeroTierOneUninstall.log
-	/bin/bash "$zthome/uninstall.sh" >/tmp/ZeroTierOneUninstall.log 2>&1
-	exit 0
-fi
+export PATH="$zthome:/bin:/usr/bin:/sbin:/usr/sbin"
 
 # Launch ZeroTier One (not as daemon... launchd monitors it)
 exec zerotier-one
--- ext/installfiles/mac/postinst.sh
+++ ext/installfiles/mac/postinst.sh
@@ -6,10 +6,12 @@ launchctl unload /Library/LaunchDaemons/com.zerotier.one.plist >>/dev/null 2>&1
 
 cd "/Library/Application Support/ZeroTier/One"
 rm -rf node.log node.log.old root-topology shutdownIfUnreadable autoupdate.log updates.d
+chown -R 0 tap.kext
+chgrp -R 0 tap.kext
 if [ ! -f authtoken.secret ]; then
 	head -c 4096 /dev/urandom | md5 | head -c 24 >authtoken.secret
-	chown root authtoken.secret
-	chgrp wheel authtoken.secret
+	chown 0 authtoken.secret
+	chgrp 0 authtoken.secret
 	chmod 0600 authtoken.secret
 fi
 rm -f zerotier-cli zerotier-idtool
