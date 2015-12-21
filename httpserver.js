23eaafc4ebf96bf44b607026308363c30740dabb
Adam Ierymenko <adam.ierymenko@gmail.com>
2015-12-21 14:01:48 -0800

    Remove stray file that is instead found in netcon.

--- httpserver.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("welcome to the machine!\n");
-});
-server.listen(8080);
-console.log("Server running!");
