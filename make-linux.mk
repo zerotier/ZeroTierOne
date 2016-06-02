f9148bf9cbcc7a88fd5d0ca459a10cf1bdbba3e7
Adam Ierymenko <adam.ierymenko@gmail.com>
2016-06-01 22:26:58 -0700

    Finally json-parser. Skip miniupnpc since some Linux distros (even newer ones) ship with an old unsafe crummy version.

--- make-linux.mk
+++ make-linux.mk
@@ -49,7 +49,11 @@ ifeq ($(wildcard /usr/include/http_parser.h),)
 else
 	LDLIBS+=-lhttp_parser
 endif
-OBJS+=ext/json-parser/json.o
+ifeq ($(wildcard /usr/include/json-parser/json.h),)
+	OBJS+=ext/json-parser/json.o
+else
+	LDLIBS+=-ljsonparser
+endif
 
 ifeq ($(ZT_OFFICIAL_RELEASE),1)
 	DEFS+=-DZT_OFFICIAL_RELEASE
@@ -58,8 +62,13 @@ endif
 
 ifeq ($(ZT_USE_MINIUPNPC),1)
 	DEFS+=-DZT_USE_MINIUPNPC -DMINIUPNP_STATICLIB -DMINIUPNPC_SET_SOCKET_TIMEOUT -DMINIUPNPC_GET_SRC_ADDR -D_BSD_SOURCE -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=600 -DOS_STRING=\"Linux\" -DMINIUPNPC_VERSION_STRING=\"1.9\" -DUPNP_VERSION_STRING=\"UPnP/1.1\" -DENABLE_STRNATPMPERR
+	OBJS+=osdep/PortMapper.o
 
-	OBJS+=ext/miniupnpc/connecthostport.o ext/miniupnpc/igd_desc_parse.o ext/miniupnpc/minisoap.o ext/miniupnpc/minissdpc.o ext/miniupnpc/miniupnpc.o ext/miniupnpc/miniwget.o ext/miniupnpc/minixml.o ext/miniupnpc/portlistingparse.o ext/miniupnpc/receivedata.o ext/miniupnpc/upnpcommands.o ext/miniupnpc/upnpdev.o ext/miniupnpc/upnperrors.o ext/miniupnpc/upnpreplyparse.o osdep/PortMapper.o
+	#ifeq ($(wildcard /usr/include/miniupnpc/miniupnpc.h),)
+		OBJS+=ext/miniupnpc/connecthostport.o ext/miniupnpc/igd_desc_parse.o ext/miniupnpc/minisoap.o ext/miniupnpc/minissdpc.o ext/miniupnpc/miniupnpc.o ext/miniupnpc/miniwget.o ext/miniupnpc/minixml.o ext/miniupnpc/portlistingparse.o ext/miniupnpc/receivedata.o ext/miniupnpc/upnpcommands.o ext/miniupnpc/upnpdev.o ext/miniupnpc/upnperrors.o ext/miniupnpc/upnpreplyparse.o
+	#else
+	#	LDLIBS+=-lminiupnpc
+	#endif
 
 	ifeq ($(wildcard /usr/include/natpmp.h),)
 		OBJS+=ext/libnatpmp/natpmp.o ext/libnatpmp/getgateway.o
@@ -131,8 +140,8 @@ selftest:	$(OBJS) selftest.o
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o zerotier-selftest selftest.o $(OBJS) $(LDLIBS)
 	$(STRIP) zerotier-selftest
 
-installer: one FORCE
-	./ext/installfiles/linux/buildinstaller.sh
+#installer: one FORCE
+#	./ext/installfiles/linux/buildinstaller.sh
 
 clean: FORCE
 	rm -rf ${GENERATED_FILES} *.so *.o netcon/*.a node/*.o controller/*.o osdep/*.o service/*.o ext/http-parser/*.o ext/lz4/*.o ext/json-parser/*.o ext/miniupnpc/*.o ext/libnatpmp/*.o $(OBJS) zerotier-one zerotier-idtool zerotier-cli zerotier-selftest zerotier-netcon-service build-* ZeroTierOneInstaller-* *.deb *.rpm .depend netcon/.depend
@@ -143,11 +152,11 @@ debug:	FORCE
 	make ZT_DEBUG=1 one
 	make ZT_DEBUG=1 selftest
 
-official: FORCE
-	make ZT_OFFICIAL_RELEASE=1 clean
-	make -j 4 ZT_OFFICIAL_RELEASE=1 one
-	make ZT_OFFICIAL_RELEASE=1 installer
-	make ZT_OFFICIAL_RELEASE=1 doc
+#official: FORCE
+#	make ZT_OFFICIAL_RELEASE=1 clean
+#	make -j 4 ZT_OFFICIAL_RELEASE=1 one
+#	make ZT_OFFICIAL_RELEASE=1 installer
+#	make ZT_OFFICIAL_RELEASE=1 doc
 
 # Includes 'doc' target
 include ${DOC_DIR}/module.mk
