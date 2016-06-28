decb4261d4e2218bbb28e0ce4e228e07782c569c
Grant Limberg <glimberg@gmail.com>
2016-06-27 19:14:15 -0700

    wire up control to launch at startup in preferences

--- ZeroTier One/PreferencesViewController.swift
+++ ZeroTier One/PreferencesViewController.swift
@@ -10,9 +10,32 @@ import Cocoa
 
 class PreferencesViewController: NSViewController {
 
+    @IBOutlet var startupCheckBox: NSButton!
+
+    let launchController = LaunchAtLoginController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+
+        if launchController.launchAtLogin {
+            startupCheckBox.state = NSOnState
+        }
+        else {
+            startupCheckBox.state = NSOffState
+        }
+    }
+
+    @IBAction func onStartupCheckBoxChanged(sender: NSButton) {
+        let bundle = NSBundle.mainBundle()
+        let bundleURL = bundle.bundleURL
+
+        if sender.state == NSOnState {
+            launchController.setLaunchAtLogin(true, forURL: bundleURL)
+        }
+        else {
+            launchController.setLaunchAtLogin(false, forURL: bundleURL)
+        }
     }
     
 }
