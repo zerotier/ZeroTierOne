fc4ea57a9cd814874c5c68e426a043b69cd3fd66
Adam Ierymenko <adam.ierymenko@gmail.com>
2016-06-30 18:03:06 -0700

    debian-stretch

--- linux-build-farm/aptly.sh
+++ linux-build-farm/aptly.sh
@@ -9,8 +9,8 @@ for distro in debian-* ubuntu-*; do
 		arches=`ls ${distro}/*.deb | cut -d _ -f 3 | cut -d . -f 1 | xargs | sed 's/ /,/g'`
 		distro_name=`echo $distro | cut -d '-' -f 2`
 		echo '---' $distro / $distro_name / $arches
-		aptly repo create -architectures=${arches} -comment="ZeroTier, Inc. Debian Packages" -component="main" -distribution=${distro_name} zt-release-${distro}
-		aptly repo add zt-release-${distro} ${distro}/*.deb
-		aptly publish repo zt-release-${distro} ${distro_name}
+		aptly repo create -architectures=${arches} -comment="ZeroTier, Inc. Debian Packages" -component="main" -distribution=${distro_name} zt-release-${distro_name}
+		aptly repo add zt-release-${distro_name} ${distro}/*.deb
+		aptly publish repo zt-release-${distro_name} $distro_name
 	fi
 done
