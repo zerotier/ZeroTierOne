62befe006d892cbfb3aefa80d8469068d0e1f39f
Grant Limberg <glimberg@gmail.com>
2016-07-26 18:21:39 -0700

    Route clicked links in the web view to the default web browser

--- ZeroTier One/AboutViewController.swift
+++ ZeroTier One/AboutViewController.swift
@@ -9,19 +9,35 @@
 import Cocoa
 import WebKit
 
-class AboutViewController: NSViewController {
+class AboutViewController: NSViewController, WebPolicyDelegate {
 
 
     @IBOutlet var webView: WebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        webView.policyDelegate = self
         // Do view setup here.
         let bundle = NSBundle.mainBundle()
         let path = bundle.URLForResource("about", withExtension: "html")
 
         if let url = path {
             webView.mainFrame.loadRequest(NSURLRequest(URL: url))
+        }
+    }
+
+
+    func webView(webView: WebView!,
+                 decidePolicyForNavigationAction actionInformation: [NSObject : AnyObject]!,
+                 request: NSURLRequest!,
+                 frame: WebFrame!,
+                 decisionListener listener: WebPolicyDecisionListener!) {
+        if (request.URL?.host) != nil {
+            NSWorkspace.sharedWorkspace().openURL(request.URL!)
+        }
+        else {
+            listener.use()
         }
     }
     
