1e6e112806903009643aef0a2f3736821c1a54d2
Adam Ierymenko <adam.ierymenko@gmail.com>
2016-07-26 17:08:09 -0700

    Dockerfile for building zerotier/zerotier-containerized for CoreOS et al.

---
+++ linux-build-farm/other/zerotier-containerized/main.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+
+if [ ! -e /dev/net/tun ]; then
+	echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
+	exit 1
+fi
+
+exec /zerotier-one
