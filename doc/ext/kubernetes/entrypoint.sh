9dc507c9ae902190f628605553f9a6b781d7e18a
Joseph Henry <josephjah@gmail.com>
2016-08-17 17:52:19 -0700

    examples files for kube int

--- doc/ext/kubernetes/entrypoint.sh
+++ doc/ext/kubernetes/entrypoint.sh
@@ -6,11 +6,15 @@ virtip4=""
 while [ -z "$virtip4" ]; do
 	sleep 0.2
 	virtip4=`/zerotier-cli listnetworks | grep -F $nwid | cut -d ' ' -f 9 | sed 's/,/\n/g' | grep -F '.' | cut -d / -f 1`
+	dev=`/zerotier-cli listnetworks | grep -F "" | cut -d ' ' -f 8 | cut -d "_" -f 2 | sed "s/^<dev>//" | tr '\n' '\0'`
 done
 echo '*** Up and running at' $virtip4 ' on network: ' $nwid
 
 echo '*** Self-Authorizing to deployment network'
-./zerotier-cli join $(NWID).conf
-./zerotier-cli net-auth $(NWID) $(DEVID)
+nwconf=$(ls *.conf)
+nwid="${nwconf%.*}"
+
+./zerotier-cli join $(nwid).conf
+./zerotier-cli net-auth $(nwid) $(dev)
 
 # node server.js
