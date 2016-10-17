7025c7090b62d3404c2ea5eda0eb35fad1d2aafa
Grant Limberg <grant.limberg@zerotier.com>
2016-10-17 14:43:24 -0700

    set proper copyright/license in code files

--- ZeroTier One/ServiceCom.m
+++ ZeroTier One/ServiceCom.m
@@ -1,10 +1,20 @@
-//
-//  ServiceCom.m
-//  ZeroTier One
-//
-//  Created by Grant Limberg on 8/4/16.
-//  Copyright © 2016 ZeroTier, Inc. All rights reserved.
-//
+/*
+ * ZeroTier One - Network Virtualization Everywhere
+ * Copyright (C) 2011-2016  ZeroTier, Inc.  https://www.zerotier.com/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "ServiceCom.h"
 #import "AuthtokenCopy.h"
