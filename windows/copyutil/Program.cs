c802811ad298d20315e358b0fc7964a16e591aa6
Grant Limberg <grant.limberg@zerotier.com>
2016-11-07 14:08:26 -0800

    Added a file copy util for Windows UI
    Copies the authtoken.secret file into a private local folder for the user so that the UI doesnt have to be run with Admin privileges.

---
+++ windows/copyutil/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyutil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Not enough arguments");
+                return;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Source directory doesn't exist!");
+                return;
+            }
+
+            Console.WriteLine("Creating: " + args[1]);
+            DirectoryInfo di = Directory.CreateDirectory(args[1]);
+
+            String authTokenSrc = args[0] + "\\authtoken.secret";
+            String authTokenDest = args[1] + "\\authtoken.secret";
+
+            String portSrc = args[0] + "\\zerotier-one.port";
+            String portDest = args[1] + "\\zerotier-one.port";
+
+            File.Copy(authTokenSrc, authTokenDest, true);
+            File.Copy(portSrc, portDest, true);
+        }
+    }
+}
+ 
