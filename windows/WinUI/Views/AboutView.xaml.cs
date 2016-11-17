a4c274085233fe085efd68869dc979ea56183c87
Grant Limberg <grant.limberg@zerotier.com>
2016-11-17 14:25:55 -0800

    added skeletons for join and about windows.  moving things around a little bit too

---
+++ windows/WinUI/Views/AboutView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WinUI
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class AboutView : Window
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
