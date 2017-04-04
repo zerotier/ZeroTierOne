6fabaae736de2daa1ba0c22701bbb11539e3fba7
Adam Ierymenko <adam.ierymenko@gmail.com>
2017-04-04 16:47:14 -0700

    Add simple root server watcher (for our own new dashboard, but also for user use).

---
+++ root-watcher/schema.sql
@@ -0,0 +1,21 @@
+/* Schema for ZeroTier root watcher log database */
+
+/* If you cluster this DB using any PG clustering scheme that uses logs, you must remove UNLOGGED here! */
+CREATE UNLOGGED TABLE "Peer"
+(
+	"ztAddress" BIGINT NOT NULL,
+	"timestamp" BIGINT NOT NULL,
+	"versionMajor" INTEGER NOT NULL,
+	"versionMinor" INTEGER NOT NULL,
+	"versionRev" INTEGER NOT NULL,
+	"rootId" INTEGER NOT NULL,
+	"phyPort" INTEGER NOT NULL,
+	"phyLinkQuality" REAL NOT NULL,
+	"phyLastReceive" BIGINT NOT NULL,
+	"phyAddress" INET NOT NULL
+);
+
+CREATE INDEX "Peer_ztAddress" ON "Peer" ("ztAddress");
+CREATE INDEX "Peer_timestamp" ON "Peer" ("timestamp");
+CREATE INDEX "Peer_rootId" ON "Peer" ("rootId");
+CREATE INDEX "Peer_phyAddress" ON "Peer" ("phyAddress");
