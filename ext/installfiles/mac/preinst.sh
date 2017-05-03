6bb855873d98309cf7ae9ed117615638366e5d8b
Adam Ierymenko <adam.ierymenko@gmail.com>
2017-05-03 15:03:26 -0700

    GitHub issue #494 -- shut down and restart UI on Mac pkg install/upgrade.

--- ext/installfiles/mac/preinst.sh
+++ ext/installfiles/mac/preinst.sh
@@ -2,6 +2,19 @@
 
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
+rm -f /tmp/zt1-gui-restart.tmp
+for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/ZeroTier,One.app' | grep -F -v grep | cut -d , -f 1,2 | xargs`; do
+	u=`echo $i | cut -d , -f 1`
+	p=`echo $i | cut -d , -f 2`
+	if [ ! -z "$u" -a "0$p" -gt 0 ]; then
+		kill $p >>/dev/null 2>&1
+		sleep 0.2
+		kill -9 $p >>/dev/null 2>&1
+		echo "$u" >>/tmp/zt1-gui-restart.tmp
+	fi
+done
+chmod 0600 /tmp/zt1-gui-restart.tmp
+
 if [ -f /Library/LaunchDaemons/com.zerotier.one.plist ]; then
 	launchctl unload /Library/LaunchDaemons/com.zerotier.one.plist >>/dev/null 2>&1
 fi
