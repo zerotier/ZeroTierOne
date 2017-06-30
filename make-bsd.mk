dd83c680e5a13ca970cf6c38f88d43119340fd05
Dave Cottlehuber <dch@skunkwerks.at>
2017-06-30 13:10:34 +0000

    build: all BSDs expect gmake here

--- make-bsd.mk
+++ make-bsd.mk
@@ -142,7 +142,7 @@ clean:
 	rm -rf *.o node/*.o controller/*.o osdep/*.o service/*.o ext/http-parser/*.o build-* zerotier-one zerotier-idtool zerotier-selftest zerotier-cli ZeroTierOneInstaller-* $(OBJS)
 
 debug:	FORCE
-	make -j 4 ZT_DEBUG=1
+	gmake -j 4 ZT_DEBUG=1
 
 install:	one
 	rm -f /usr/local/sbin/zerotier-one
