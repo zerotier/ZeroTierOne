574b24c0826cea575b77f36aa0238d7a26aeac7b
Adam Ierymenko <adam.ierymenko@gmail.com>
2018-03-08 22:41:42 -0800

    docs

--- controller/EmbeddedNetworkController.cpp
+++ controller/EmbeddedNetworkController.cpp
@@ -1,6 +1,6 @@
 /*
  * ZeroTier One - Network Virtualization Everywhere
- * Copyright (C) 2011-2015  ZeroTier, Inc->
+ * Copyright (C) 2011-2018  ZeroTier, Inc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
