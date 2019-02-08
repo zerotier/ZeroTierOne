3d21f0a91f40afedd4411f857f4df1c9429416f3
Grant Limberg <grant.limberg@zerotier.com>
2019-02-08 10:40:57 -0800

    update docker registry used

--- make-linux.mk
+++ make-linux.mk
@@ -322,7 +322,7 @@ central-controller:	FORCE
 	make -j4 LDLIBS="-L/usr/pgsql-10/lib/ -lpq" CXXFLAGS="-I/usr/pgsql-10/include -fPIC" DEFS="-DZT_CONTROLLER_USE_LIBPQ -DZT_CONTROLLER" ZT_OFFICIAL=1 ZT_USE_X64_ASM_ED25519=1 one
 
 central-controller-docker:	central-controller
-	docker build -t gcr.io/zerotier-central/ztcentral-controller:${TIMESTAMP} -f docker/Dockerfile . 
+	docker build -t docker.zerotier.com/zerotier-central/ztcentral-controller:${TIMESTAMP} -f docker/Dockerfile . 
 
 debug:	FORCE
 	make ZT_DEBUG=1 one
--- update_controllers.sh
+++ update_controllers.sh
@@ -2,17 +2,25 @@
 
 if [ -z "$1" ]
 then
-    echo "Must supply a docker tag"
+    echo "Usage: $0 <docker_tag> <k8s_namespace>"
+    echo "    k8s_namesapce is set to default if not specified"
     exit 1
+fi
+
+if [ -z "$2" ]
+then
+    NAMESPACE=default
+else
+    NAMESPACE=$2
 fi
 
 TAG=$1
 
-CONTROLLERS=`kubectl get pods -o=name | grep controller | sed "s/^.\{4\}//" | cut -d '-' -f 2`
+CONTROLLERS=`kubectl get pods -o=name --namespace=${NAMESPACE} | grep controller | sed "s/^.\{4\}//" | cut -d '-' -f 2`
 
 for c in ${CONTROLLERS[@]}
 do
-    kubectl set image deployment controller-${c} ztcentral-controller=gcr.io/zerotier-central/ztcentral-controller:${TAG}
+    kubectl set image deployment --namespace=${NAMESPACE} controller-${c} ztcentral-controller=docker.zerotier.com/zerotier-central/ztcentral-controller:${TAG}
     echo "Sleeping for 1 minute"
     sleep 60
 done
