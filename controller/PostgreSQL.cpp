37d321afc80aaced7e0473e33893787d9a42ef78
Grant Limberg <grant.limberg@zerotier.com>
2019-05-31 15:10:45 -0700

    dont spin as much in the onlineNotificationThread

--- controller/PostgreSQL.cpp
+++ controller/PostgreSQL.cpp
@@ -1512,7 +1512,7 @@ void PostgreSQL::onlineNotificationThread()
 		// 	PQclear(res);
 		// }
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(0));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 	fprintf(stderr, "%s: Fell out of run loop in onlineNotificationThread\n", _myAddressStr.c_str());
 	PQfinish(conn);
