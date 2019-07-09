fee6aae442d86146063fa903626a95f7adcf8764
Adam Ierymenko <adam.ierymenko@gmail.com>
2019-07-09 10:31:08 -0400

    Drop in faster C25519 agreement code.

--- attic/world/build.sh
+++ attic/world/build.sh
@@ -1,1 +1,3 @@
-c++ -I.. -o mkworld ../node/C25519.cpp ../node/Salsa20.cpp ../node/SHA512.cpp ../node/Identity.cpp ../node/Utils.cpp ../node/InetAddress.cpp ../osdep/OSUtils.cpp mkworld.cpp
+#!/bin/bash
+
+c++ -std=c++11 -I../.. -I.. -O -o mkworld ../../node/C25519.cpp ../../node/Salsa20.cpp ../../node/SHA512.cpp ../../node/Identity.cpp ../../node/Utils.cpp ../../node/InetAddress.cpp ../../osdep/OSUtils.cpp mkworld.cpp -lm
