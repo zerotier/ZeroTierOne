5b7d60f5cdd9157cb7d0beb3a82a2ceee7ee4c9c
Joseph Henry <josephjah@gmail.com>
2019-08-13 14:42:48 -0700

    Whoops

--- node/Trace.cpp
+++ node/Trace.cpp
@@ -109,10 +109,10 @@ void Trace::peerConfirmingUnknownPath(void *const tPtr,const uint64_t networkId,
 
 void Trace::peerLinkNowRedundant(void *const tPtr,Peer &peer)
 {
-	if ((RR->node->getMultipathMode() != ZT_MULTIPATH_RANDOM)) {
+	if ((RR->node->getMultipathMode() == ZT_MULTIPATH_RANDOM)) {
 		ZT_LOCAL_TRACE(tPtr,RR,"link to peer %.10llx is now a randomly-distributed aggregate link",peer.address().toInt());
 	}
-	if ((RR->node->getMultipathMode() != ZT_MULTIPATH_PROPORTIONALLY_BALANCED)) {
+	if ((RR->node->getMultipathMode() == ZT_MULTIPATH_PROPORTIONALLY_BALANCED)) {
 		ZT_LOCAL_TRACE(tPtr,RR,"link to peer %.10llx is now a proportionally-balanced aggregate link",peer.address().toInt());
 	}
 }
