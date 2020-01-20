b5982632610e27a9d107a3f52936d582994a3474
Adam Ierymenko <adam.ierymenko@zerotier.com>
2020-01-20 08:49:10 -0800

    More 2.x notice updates.

--- osdep/EthernetTap.cpp
+++ osdep/EthernetTap.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (c)2019 ZeroTier, Inc.
+ * Copyright (c)2013-2020 ZeroTier, Inc.
  *
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file in the project's root directory.
  *
- * Change Date: 2023-01-01
+ * Change Date: 2024-01-01
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2.0 of the Apache License.
@@ -14,8 +14,8 @@
 #include "EthernetTap.hpp"
 #include "OSUtils.hpp"
 
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
 
 #ifdef ZT_SDK
 
