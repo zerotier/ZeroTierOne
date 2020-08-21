7f99c4a7793b8c0621b12f0f1ad57d62b0b3dbc9
Grant Limberg <grant.limberg@zerotier.com>
2020-08-21 16:22:28 -0700

    Sleep 10 seconds between writes to DB

--- controller/PostgreSQL.cpp
+++ controller/PostgreSQL.cpp
@@ -1714,7 +1714,7 @@ void PostgreSQL::onlineNotification_Redis()
 			fprintf(stderr, "Error in online notification thread (redis): %s\n", e.what());
 #endif
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::seconds(10));
 	}
 }
 
