93d6b41898665b6df42b23d0a04b2b5e34668f12
Adam Ierymenko <adam.ierymenko@zerotier.com>
2020-08-25 08:08:54 -0700

    Disable AES in commit so as not to break other builds.

--- node/Peer.hpp
+++ node/Peer.hpp
@@ -534,7 +534,10 @@ public:
 	inline int8_t bondingPolicy() { return _bondingPolicy; }
 
 	const AES *aesKeysIfSupported() const
-	{ return (_vProto >= 10) ? _aesKeys : (const AES *)0; }
+	{ return (const AES *)0; }
+
+	//const AES *aesKeysIfSupported() const
+	//{ return (_vProto >= 10) ? _aesKeys : (const AES *)0; }
 
 private:
 	struct _PeerPath
