53d5c9890f94d32715b637a650f09da0e4a7f936
Grant Limberg <grant.limberg@zerotier.com>
2020-08-26 15:50:36 -0700

    missed a rollback
    # Conflicts:
    #	controller/PostgreSQL.cpp

--- controller/PostgreSQL.cpp
+++ controller/PostgreSQL.cpp
@@ -4,7 +4,7 @@
  * Use of this software is governed by the Business Source License included
  * in the LICENSE.TXT file in the project's root directory.
  *
- * Change Date: 2023-01-01
+ * Change Date: 2025-01-01
  *
  * On the date above, in accordance with the Business Source License, use
  * of this software will be governed by version 2.0 of the Apache License.
@@ -1361,6 +1361,7 @@ void PostgreSQL::commitThread()
 						PQclear(res);
 					}
 					if (err) {
+						PQclear(res);
 						PQclear(PQexec(conn, "ROLLBACK"));
 						delete config;
 						config = nullptr;
