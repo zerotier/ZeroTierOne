0e8b54f7a1ff2b064c5384260121074d2ac1731d
Joseph Henry <josephjah@gmail.com>
2020-09-11 14:47:18 -0700

    Add minor trace output formatting changes. Change ZT_MULTIPATH_BOND_STATUS_INTERVAL from 30000 to 60000

--- node/Constants.hpp
+++ node/Constants.hpp
@@ -396,7 +396,7 @@
 /**
  * How often we emit a one-liner bond summary for each peer
  */
-#define ZT_MULTIPATH_BOND_STATUS_INTERVAL 30000
+#define ZT_MULTIPATH_BOND_STATUS_INTERVAL 60000
 
 /**
  * How long before we consider a path to be dead in the general sense. This is
