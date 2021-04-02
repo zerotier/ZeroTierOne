38fcefceeea6ef9ff953d37523cd95f7fc76f9c1
Adam Ierymenko <adam.ierymenko@zerotier.com>
2021-04-02 15:32:12 -0400

    Small fix for VAES.

--- core/AES_aesni.cpp
+++ core/AES_aesni.cpp
@@ -429,7 +429,7 @@ void AES::CTR::p_aesNICrypt(const uint8_t *in, uint8_t *out, unsigned int len) n
 	if (likely(len >= 64)) {
 
 #if defined(ZT_AES_VAES512) && defined(ZT_AES_VAES256)
-		if (Utils::CPUID.vaes && (len >= 256)) {
+		if (Utils::CPUID.vaes) {
 			if (Utils::CPUID.avx512f) {
 				p_aesCtrInnerVAES512(len, _ctr[0], c1, in, out, k);
 			} else {
@@ -441,9 +441,9 @@ void AES::CTR::p_aesNICrypt(const uint8_t *in, uint8_t *out, unsigned int len) n
 
 #if !defined(ZT_AES_VAES512) && defined(ZT_AES_VAES256)
 		if (Utils::CPUID.vaes && (len >= 256)) {
-				p_aesCtrInnerVAES256(len, _ctr[0], c1, in, out, k);
-				goto skip_conventional_aesni_64;
-			}
+			p_aesCtrInnerVAES256(len, _ctr[0], c1, in, out, k);
+			goto skip_conventional_aesni_64;
+		}
 #endif
 
 		const uint8_t *const eof64 = in + (len & ~((unsigned int)63));
