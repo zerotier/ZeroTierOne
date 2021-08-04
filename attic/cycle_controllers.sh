653a6dcf3c22b60e42030d9cd828bd977936f39f
Adam Ierymenko <adam.ierymenko@zerotier.com>
2021-08-04 12:36:15 -0400

    Huge cleanup. Some of these things will come back but right now clean them out of this branch.

--- attic/cycle_controllers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONTROLLERS=`kubectl get pods -o=name | grep controller | sed "s/^.\{4\}//"`
-
-for c in ${CONTROLLERS[@]}
-do
-    kubectl delete pod ${c}
-    sleep 30
-done
--- installsupport/mac/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH="/Library/Application Support/ZeroTier/One:/bin:/usr/bin:/sbin:/usr/sbin"
-exec zerotier-one
