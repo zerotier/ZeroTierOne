ae93cccde69df0a06f6a3a8016ec3c3fddfd83d2
Shawn <sbc19861004@aliyun.com>
2022-01-27 11:59:21 +0800

    FIX: fix wrong flag usage of rateGateCredentialsReceived

--- node/Peer.hpp
+++ node/Peer.hpp
@@ -391,7 +391,7 @@ public:
 			++_credentialsCutoffCount;
 		else _credentialsCutoffCount = 0;
 		_lastCredentialsReceived = now;
-		return (_directPathPushCutoffCount < ZT_PEER_CREDEITIALS_CUTOFF_LIMIT);
+		return (_credentialsCutoffCount < ZT_PEER_CREDEITIALS_CUTOFF_LIMIT);
 	}
 
 	/**
