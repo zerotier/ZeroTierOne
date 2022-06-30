a6d298e536b19270f001b1b7435723c7a50272f1
Adam Ierymenko <adam.ierymenko@zerotier.com>
2022-06-30 15:03:03 -0400

    Lock goes in root now.

--- zerotier-network-hypervisor/src/util/testutil.rs
+++ zerotier-network-hypervisor/src/util/testutil.rs
@@ -1,3 +1,5 @@
+// (c) 2020-2022 ZeroTier, Inc. -- currently propritery pending actual release and licensing. See LICENSE.md.
+
 // from zeronsd
 pub fn randstring(len: u8) -> String {
     (0..len).map(|_| (rand::random::<u8>() % 26) + 'a' as u8).map(|c| if rand::random::<bool>() { (c as char).to_ascii_uppercase() } else { c as char }).map(|c| c.to_string()).collect::<Vec<String>>().join("")
