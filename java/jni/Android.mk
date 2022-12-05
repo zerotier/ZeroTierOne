cdf248b1e2333f914fcecb8dabc651914f2a65d7
Brenton Bostick <bostick@gmail.com>
2022-12-05 09:56:13 -0500

    Fix build problem related to unified headers
    Since NDKr15 (released 2017), unified headers are used by default [1]
    
    Remove -isystem option that was passing bad values to command-line.
    
    The actual value being passed to command-line was:
    ```
    -isystem DK/sysroot/usr/include/RIPLE
    ```
    
    because of using $NDK and $TRIPLE instead of $(NDK) and $(TRIPLE)
    
    But regardless, $NDK and $TRIPLE were never actually defined values and were just
    place-holders mentioned in [1]
    
    [1] https://android.googlesource.com/platform/ndk/+/ndk-release-r16/docs/UnifiedHeaders.md

--- java/jni/Android.mk
+++ java/jni/Android.mk
@@ -11,15 +11,13 @@ LOCAL_C_INCLUDES := \
 LOCAL_LDLIBS := -llog
 # LOCAL_CFLAGS := -g
 
-APP_UNIFIED_HEADERS := true
-
 LOCAL_CFLAGS := -DZT_USE_MINIUPNPC
 ifeq ($(TARGET_ARCH_ABI),x86_64)
     LOCAL_CXXFLAGS := -maes -mpclmul -msse3 -msse4.1
 endif
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
     LOCAL_ARM_NEON := true
-    LOCAL_CXXFLAGS := -march=armv8-a+crypto -mfloat-abi=softfp -mfpu=neon -maes -isystem $NDK/sysroot/usr/include/$TRIPLE
+    LOCAL_CXXFLAGS := -march=armv8-a+crypto -mfloat-abi=softfp -mfpu=neon -maes
 endif
 
 # ZeroTierOne SDK source files
