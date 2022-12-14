b115abb65947bf69ee6865d95bb80d8e01dd6a0b
mamoniot <mamoniot@protonmail.com>
2022-12-14 14:54:17 -0500

    made constants public

--- zssp/src/lib.rs
+++ zssp/src/lib.rs
@@ -1,9 +1,9 @@
 
-mod constants;
 mod zssp;
 mod app_layer;
 mod ints;
 
+pub mod constants;
 pub use zssp::{Error, ReceiveResult, ReceiveContext, Session};
 pub use app_layer::ApplicationLayer;
 pub use ints::{SessionId, Role};
