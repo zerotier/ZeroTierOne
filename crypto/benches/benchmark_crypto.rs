f8aa44082ea211e51a91745e2c19811654b895d3
Adam Ierymenko <adam.ierymenko@zerotier.com>
2023-02-23 18:06:19 -0500

    warning removal

--- crypto/benches/benchmark_crypto.rs
+++ crypto/benches/benchmark_crypto.rs
@@ -2,15 +2,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::time::Duration;
 
 use zerotier_crypto::p384::*;
-use zerotier_crypto::random;
 use zerotier_crypto::x25519::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let p384_a = P384KeyPair::generate();
     let p384_b = P384KeyPair::generate();
-
-    //let kyber_a = pqc_kyber::keypair(&mut random::SecureRandom::default());
-    //let kyber_encap = pqc_kyber::encapsulate(&kyber_a.public, &mut random::SecureRandom::default()).unwrap();
 
     let x25519_a = X25519KeyPair::generate();
     let x25519_b = X25519KeyPair::generate();
@@ -23,12 +19,6 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| p384_a.agree(p384_b.public_key()).expect("ecdhp384 failed"))
     });
     group.bench_function("ecdhx25519", |b| b.iter(|| x25519_a.agree(&x25519_b_pub)));
-    //group.bench_function("kyber_encapsulate", |b| {
-    //    b.iter(|| pqc_kyber::encapsulate(&kyber_a.public, &mut random::SecureRandom::default()).expect("kyber encapsulate failed"))
-    //});
-    //group.bench_function("kyber_decapsulate", |b| {
-    //    b.iter(|| pqc_kyber::decapsulate(&kyber_encap.0, &kyber_a.secret).expect("kyber decapsulate failed"))
-    //});
 
     group.finish();
 }
