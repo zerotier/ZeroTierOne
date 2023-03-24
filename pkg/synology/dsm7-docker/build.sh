a8fde9e530bd21a611a83a8222bc395b380556df
Joseph Henry <joseph.henry@zerotier.com>
2023-03-24 11:58:34 -0700

    Minor tweak to Synology Docker image script (#1936)

--- pkg/synology/dsm7-docker/build.sh
+++ pkg/synology/dsm7-docker/build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-ZTO_VER=$(git describe --abbrev=0 --tags)
+ZTO_VER=$(git describe --tags $(git rev-list --tags --max-count=1))
 ZTO_COMMIT=$(git rev-parse HEAD)
 
-build-and-push()
+build()
 {
-  # build
-
   sudo docker build --load --rm -t zerotier-synology . --build-arg ZTO_COMMIT=${ZTO_COMMIT} --build-arg ZTO_VER=${ZTO_VER}
   LATEST_DOCKER_IMAGE_HASH=$(sudo docker images -q zerotier-synology)
   sudo docker tag ${LATEST_DOCKER_IMAGE_HASH} zerotier/zerotier-synology:${ZTO_VER}
   sudo docker tag ${LATEST_DOCKER_IMAGE_HASH} zerotier/zerotier-synology:latest
+}
 
-  # push
-
+push()
+{
   sudo docker login --username=${DOCKERHUB_USERNAME}
   sudo docker push zerotier/zerotier-synology:${ZTO_VER}
   sudo docker push zerotier/zerotier-synology:latest
