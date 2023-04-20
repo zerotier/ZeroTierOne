2d41b825e4551b5a4eea51c620a8d864b594f5bf
Grant Limberg <grant.limberg@zerotier.com>
2023-04-20 13:44:20 -0700

    add copyright headers to new files

--- node/Metrics.hpp
+++ node/Metrics.hpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c)2013-2023 ZeroTier, Inc.
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file in the project's root directory.
+ *
+ * Change Date: 2025-01-01
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by version 2.0 of the Apache License.
+ */
 #ifndef METRICS_H_
 #define METRICS_H_
 
