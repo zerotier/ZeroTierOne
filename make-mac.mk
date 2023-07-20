74d98c931da3b8ae6bfc0ce2c9e6df631cb6e956
Grant Limberg <grant.limberg@zerotier.com>
2023-07-20 16:21:30 -0700

    fix docker tag for controllers

--- make-mac.mk
+++ make-mac.mk
@@ -184,8 +184,8 @@ _buildx:
 	@echo docker buildx inspect --bootstrap
 
 central-controller-docker: _buildx FORCE
-	docker buildx build --platform linux/arm64,linux/amd64 --no-cache -t registry.zerotier.com/zerotier/ztcentral-controller:${TIMESTAMP} -f ext/central-controller-docker/Dockerfile --build-arg git_branch=$(shell git name-rev --name-only HEAD) . --push
-	@echo Image: registry.zerotier.com/zerotier/ztcentral-controller:${TIMESTAMP}
+	docker buildx build --platform linux/arm64,linux/amd64 --no-cache -t registry.zerotier.com/zerotier-central/ztcentral-controller:${TIMESTAMP} -f ext/central-controller-docker/Dockerfile --build-arg git_branch=$(shell git name-rev --name-only HEAD) . --push
+	@echo Image: registry.zerotier.com/zerotier-central/ztcentral-controller:${TIMESTAMP}
 	
 clean:
 	rm -rf MacEthernetTapAgent *.dSYM build-* *.a *.pkg *.dmg *.o node/*.o controller/*.o service/*.o osdep/*.o ext/http-parser/*.o $(CORE_OBJS) $(ONE_OBJS) zerotier-one zerotier-idtool zerotier-selftest zerotier-cli zerotier doc/node_modules zt1_update_$(ZT_BUILD_PLATFORM)_$(ZT_BUILD_ARCHITECTURE)_* zeroidc/target/
