29ef10e759399068e3bbb84837afe3eec79bdbf4
Adam Ierymenko <adam.ierymenko@zerotier.com>
2024-09-27 17:00:38 -0400

    Bump version to an intermediate (odd) number.

--- version.h
+++ version.h
@@ -22,12 +22,12 @@
 /**
  * Minor version
  */
-#define ZEROTIER_ONE_VERSION_MINOR 14
+#define ZEROTIER_ONE_VERSION_MINOR 15
 
 /**
  * Revision
  */
-#define ZEROTIER_ONE_VERSION_REVISION 1
+#define ZEROTIER_ONE_VERSION_REVISION 0
 
 /**
  * Build version
